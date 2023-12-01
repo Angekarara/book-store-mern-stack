d9e1e15d63118776ba2fd8ba9d3affec0c21a798
Angekarara <mbabange2020@gmail.com>
2023-12-01 08:48:44 +0200

    book model

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModels.js";
 
 const app = express();
 
