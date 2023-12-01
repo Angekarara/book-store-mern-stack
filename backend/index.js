67f1101b4511b849124497ac26084922e7e36d49
Angekarara <mbabange2020@gmail.com>
2023-12-01 08:54:07 +0200

    book model

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,14 @@ import { Book } from "./models/bookModels.js";
 const app = express();
 
 app.get("/", (request, response) => {
+  try {
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send("welcome to mern stack tutorial");
+  }
+});
+
+app.post("/books", async (request, response) => {
   console.log(request);
   return response.status(234).send("welcome to Mern Stack tutorial");
 });
