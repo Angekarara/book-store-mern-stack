21fc09f22f3de95305988175a8c7c6086b68e0b9
Angekarara <mbabange2020@gmail.com>
2024-02-13 14:03:28 +0200

    new changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -31,20 +31,20 @@ const Home = () => {
     <div className="p-4">
       <div className="flex justify-center items-center gap-x-4">
         <button
-          className="bg-sky-300 hover:bg-sky-900 px-4 py-1 rounded-lg"
+          className="bg-green-900 hover:bg-gray-800 px-4 py-1 rounded-lg"
           onClick={() => setShowType("table")}
         >
           Table
         </button>
         <button
-          className="bg-sky-300 hover:bg-sky-900 px-4 py-1 rounded-lg"
+          className="bg-green-900 hover:bg-gray-800 px-4 py-1 rounded-lg"
           onClick={() => setShowType("card")}
         >
           Card
         </button>
       </div>
       <div className="flex justify-between items-center">
-        <h1 className="text-3xl my-8">Books List</h1>
+        <h1 className="text-3xl my-8 text-blue-900">Books List</h1>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
